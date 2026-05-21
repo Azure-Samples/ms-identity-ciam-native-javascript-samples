e341eeba187ed33962b5a78753d6904f3aca8f33
Jian Shen <45603194+shenj@users.noreply.github.com>
2026-05-21 15:47:34 +0100

    Support custom header injection (#62)
    * Support custom header injection
    
    * Upgrade package

--- typescript/native-auth/react-nextjs-sample/src/config/auth-config.ts
+++ typescript/native-auth/react-nextjs-sample/src/config/auth-config.ts
@@ -1,20 +1,34 @@
 import { CustomAuthConfiguration, LogLevel } from "@azure/msal-browser/custom-auth";
+import type { CustomAuthRequestInterceptor } from "@azure/msal-browser/custom-auth";
+
+const requestInterceptor: CustomAuthRequestInterceptor = {
+    addAdditionalHeaderFields(requestUrl: URL) {
+        if (requestUrl.pathname.endsWith("/oauth2/v2.0/initiate")) {
+            return {
+                value_1: "customer_header_1", // Will be ignored: doesn't start with "x-"
+                "x-client-header": "customer_header_2", // Will be ignored: starts with reserved prefix "x-client-"
+                "X-my-custom-header": "my data", // Will be added to the network request
+            };
+        }
+
+        return null;
+    },
+};
 
 export const customAuthConfig: CustomAuthConfiguration = {
     customAuth: {
         challengeTypes: ["password", "oob", "redirect"],
         authApiProxyUrl: "http://localhost:3001/api",
+        requestInterceptor: requestInterceptor,
     },
     auth: {
         clientId: "Enter_the_Application_Id_Here",
         authority: "https://Enter_the_Tenant_Subdomain_Here.ciamlogin.com",
         redirectUri: "/",
         postLogoutRedirectUri: "",
-        navigateToLoginRequestUrl: false,
     },
     cache: {
         cacheLocation: "sessionStorage",
-        storeAuthStateInCookie: false,
     },
     system: {
         loggerOptions: {
